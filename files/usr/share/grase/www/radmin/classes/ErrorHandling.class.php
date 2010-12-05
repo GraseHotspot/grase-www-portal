341e2f697e4ee3806db1fcf4bfbb083ca63b8376
Timothy White <tim@purewhite.id.au>
2010-12-05 19:28:54 +1000

    Added DB_error function that logs more details about DB errors to Adminlog

--- files/usr/share/grase/www/radmin/classes/ErrorHandling.class.php
+++ files/usr/share/grase/www/radmin/classes/ErrorHandling.class.php
@@ -10,7 +10,7 @@ class ErrorHandling
         $AdminLog =& AdminLog::getInstance();
         $AdminLog->log_error($error);
         
-        require('smarty/Smarty.class.php');
+        require_once('smarty/Smarty.class.php');
         //require_once 'libs/Smarty.class.php';
 
         $smarty = new Smarty;
@@ -23,6 +23,26 @@ class ErrorHandling
         die();
 
     }
+    
+    // ErrorHandling::fatal_db_error
+    public function fatal_db_error($error, $pear_error_obj)
+    {
+        $AdminLog =& AdminLog::getInstance();
+        $AdminLog->log_error($error . $pear_error_obj->toString());
+        
+        require_once('smarty/Smarty.class.php');
+        //require_once 'libs/Smarty.class.php';
+
+        $smarty = new Smarty;
+
+        $smarty->compile_check = true;
+        $smarty->assign("Application", APPLICATION_NAME);
+        $smarty->assign("error", $error . $pear_error_obj->getMessage());
+        
+        $smarty->display("error.tpl");
+        die();
+
+    }    
     
     
     // ErrorHandling::fatal_nodb_error
