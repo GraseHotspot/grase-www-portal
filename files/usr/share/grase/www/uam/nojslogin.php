e8c7d8838e97542135fe76dc7d7e057d52a7b490
Timothy White <tim@purewhite.id.au>
2011-03-17 11:05:08 +1000

    trying to get CHAP md5 stuff working

--- files/usr/share/grase/www/uam/nojslogin.php
+++ files/usr/share/grase/www/uam/nojslogin.php
@@ -10,13 +10,27 @@ $username = urlencode($_POST['username']);
 $password = $_POST['password'];
 $challenge = $_POST['challenge'];
 $userurl = urlencode($_POST['userurl']);
+$ident = '00';
 
 if (! ( $username && $password && $challenge) )
 {
     header("Location: http://10.1.0.1:3990/prelogin");
 }
-$response = md5("\0" . $password . $challenge);
+$hexchal = pack ("H32", $challenge);
+$response = md5("\0" . $password . $hexchal);
+//print md5($ident . String2Hex($password) . $hexchal);
+
 $challenge = urlencode($challenge);
 header("Location: http://10.1.0.1:3990/login?username=$username&response=$response&userurl=$userurl");
+
+
+/*function String2Hex($string){
+    $hex='';
+    for ($i=0; $i < strlen($string); $i++){
+        $hex .= dechex(ord($string[$i]));
+    }
+    return $hex;
+}*/
+
 ?>
 
