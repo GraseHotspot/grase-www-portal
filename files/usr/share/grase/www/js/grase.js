f7f340e349422201acf64ed2e74b94ceda59b0c9
Timothy White <tim@purewhite.id.au>
2011-03-24 20:21:43 +1000

    Jquery related css and new global js includes

---
+++ files/usr/share/grase/www/js/grase.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    // Simple jQuery to create help dialogs out of all elements with class helpbutton and a title attribute		
+	$('.helpbutton[title]').each(function(index, element){
+	    content = $(element).attr('title');
+	    $(element).attr('title', '');
+	    createHelpDialog('helpdialog' + index, 'Help', content);
+	    $(element).click(function()
+	    {
+	        $('#helpdialog'+index).dialog('open');
+	    })
+	});
+	
+
+}) ;
+
+function createHelpDialog(id, title, content)
+{
+    // Simple function that uses jQuery UI to create a (Closed) dialog box for later use
+    $("<div/>", {
+        id: id,
+        html: content
+    }).dialog({
+        autoOpen: false,
+        title: title,
+        resizable: false,        
+    })
+}
+
