7e30b2932892dbcf7b8e2548c747c32f91f5bc4a
Timothy White <tim@purewhite.id.au>
2011-05-04 17:46:40 +1000

    With dynamic groups, also show All group in new tab interface

--- files/usr/share/grase/www/radmin/display.php
+++ files/usr/share/grase/www/radmin/display.php
@@ -26,8 +26,11 @@ require_once 'includes/database_functions.inc.php';
 
 	$users = database_get_users(database_get_user_names());
 	$users_groups = sort_users_into_groups($users); // TODO: Reports and then no longer sort user list by downloads??
+	$users_groups['All'] = $users; // TODO: Group names can't have space in name TODO: Translate all?
+
 	$smarty->assign("users", $users);
 	$smarty->assign("users_groups", $users_groups);
 	$smarty->register_modifier( "sortby", "smarty_modifier_sortby" );   
 	display_page('display.tpl');
+
 ?>
