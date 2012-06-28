bae0ff22a58d3598450ad7f3cc1ef23fad92cee1
Tim White <tim@mint>
2012-06-28 17:53:49 +1000

    remove debugging var_dump that was left in

--- files/usr/share/grase/www/radmin/includes/misc_functions.inc.php
+++ files/usr/share/grase/www/radmin/includes/misc_functions.inc.php
@@ -243,7 +243,7 @@ function validate_uucptimerange($timeranges)
         foreach($timerange as $range)
         {
             $result = preg_match('/^(Su|Mo|Tu|We|Th|Fr|Sa|Sun|Mon|Tue|Wed|Thur|Fri|Sat|Wk|Any|Al|Never)(\d{4}-\d{4})?$/', $range);
-            var_dump(array($range, $result));        
+            //var_dump(array($range, $result));        
             if($result == 0)
                 return T_('Invalid Time Range ' . $timeranges);
         }
