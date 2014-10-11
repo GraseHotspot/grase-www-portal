9f994d69fe22e90a9174875bb3ef7b74997dc5c8
Tim White <tim@whiteitsolutions.com.au>
2014-10-11 14:41:38 +1000

    Formatting

--- files/usr/share/grase/www/uam/includes/site.inc.php
+++ files/usr/share/grase/www/uam/includes/site.inc.php
@@ -3,19 +3,19 @@
 /* Copyright 2008 Timothy White */
 
 // Page loading time
-   $mtime = microtime();
-   $mtime = explode(" ",$mtime);
-   $mtime = $mtime[1] + $mtime[0];
-   $pagestarttime = $mtime; 
+$mtime = microtime();
+$mtime = explode(" ", $mtime);
+$mtime = $mtime[1] + $mtime[0];
+$pagestarttime = $mtime;
 
 // Settings
 
 require_once "MDB2.php";
 
-function grase_autoload($class_name) {
-    if( file_exists(__DIR__. '/../../radmin/classes/' . $class_name . '.class.php'))
-    {
-        include_once __DIR__. '/../../radmin/classes/' . $class_name . '.class.php';
+function grase_autoload($class_name)
+{
+    if (file_exists(__DIR__ . '/../../radmin/classes/' . $class_name . '.class.php')) {
+        include_once __DIR__ . '/../../radmin/classes/' . $class_name . '.class.php';
     }
 }
 
@@ -25,7 +25,7 @@ spl_autoload_register('grase_autoload');
 require_once('../radmin/includes/load_settings.inc.php');
 require_once('../radmin/includes/misc_functions.inc.php');
 
-require_once __DIR__.'/../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 //require_once '../radmin/includes/block.t.php';
 
 $smarty = new SmartyBC();
@@ -36,10 +36,13 @@ $smarty = new SmartyBC();
 #$smarty->register_block('t', 'smarty_block_t');
 
 $smarty->assign("Location", $Settings->getSetting('locationName'));
-$smarty->assign("Support", array(
+$smarty->assign(
+    "Support",
+    array(
         "link" => $Settings->getSetting('supportContactLink'),
         "name" => $Settings->getSetting('supportContactName')
-    ));
+    )
+);
 $smarty->assign("website_name", $Settings->getSetting('websiteName'));
 $smarty->assign("website_link", $Settings->getSetting('websiteLink'));
 
@@ -51,7 +54,9 @@ $smarty->assign("serverip", $lanIP);
 custom_settings(array('hidefooter', 'hideheader', 'disableallcss', 'hidehelplink', 'hidelogoutbookmark'));
 
 $logintitle = $Settings->getSetting('logintitle');
-if($logintitle == '') $logintitle = $Settings->getSetting('locationName') ." Hotspot";
+if ($logintitle == '') {
+    $logintitle = $Settings->getSetting('locationName') . " Hotspot";
+}
 $smarty->assign("logintitle", $logintitle);
 
 // Load templates needed by all pages
@@ -60,20 +65,18 @@ load_templates(array('maincss'));
 function load_templates($templates)
 {
     global $Settings, $smarty;
-    foreach($templates as $template)
-    {
-        $smarty->assign('tpl_'.$template, $Settings->getTemplate($template));
+    foreach ($templates as $template) {
+        $smarty->assign('tpl_' . $template, $Settings->getTemplate($template));
     }
 }
 
 function custom_settings($settings = array())
 {
     global $smarty, $Settings;
-    foreach($settings as $setting)
-    {
-        $smarty->assign($setting, $Settings->getSetting($setting) == 'TRUE'
-                ? TRUE : FALSE);
+    foreach ($settings as $setting) {
+        $smarty->assign(
+            $setting,
+            $Settings->getSetting($setting) == 'TRUE' ? true : false
+        );
     }
 }
-
-?>
