676b2cbb976e461cc299fe49268f9ae7d50b6dee
Tim White <tim@whiteitsolutions.com.au>
2014-10-13 22:31:24 +1000

    Move to NOWDOCs for template defaults

--- files/usr/share/grase/src/Grase/Database/Upgrade.php
+++ files/usr/share/grase/src/Grase/Database/Upgrade.php
@@ -212,14 +212,15 @@ class Upgrade
             // loginhelptext: displayed above login form in main portal page
             $Settings->setTemplate(
                 'loginhelptext',
-                '
-                <p>By logging in, you are agreeing to the following:</p>
-                <ul>
-                    <li><strong>All network activity will be monitored, this includes: websites, bandwidth usage, protocols</strong></li>
-                    <li><strong>You will not access sites containing explicit or inappropriate material</strong></li>
-                    <li><strong>You will not attempt to access any system on this network</strong></li>
-                </ul>
-            '
+                <<<'EOT'
+<p>By logging in, you are agreeing to the following:</p>
+<ul>
+    <li><strong>All network activity will be monitored, this includes: websites, bandwidth usage, protocols</strong></li>
+    <li><strong>You will not access sites containing explicit or inappropriate material</strong></li>
+    <li><strong>You will not attempt to access any system on this network</strong></li>
+</ul>
+EOT
+
             );
             $this->rowsUpdated++;
         }
@@ -229,19 +230,20 @@ class Upgrade
         if ($Settings->getTemplate('helptext') === null) {
             $Settings->setTemplate(
                 'helptext',
-                '<p>For payment and an account, please contact the Office during office hours.</p>
-                <p>For a quick logout, bookmark <a href="http://10.1.0.1:3990/logoff">LOGOUT</a>, this link will instantly log you out, and return you to the Welcome page.<br/>
-                To get back to the status page, bookmark ether the Non javascript version (<a href="./nojsstatus" target="grasestatus">Hotspot Status nojs</a>), or the preferred javascript version (<a href="javascript: loginwindow = window.open("http://10.1.0.1/grase/uam/mini", "grasestatus", "width=300,height=400,location=no,directories=no,status=yes,menubar=no,toolbar=no"); loginwindow.focus();">Hotspot Status</a>). You can just drag ether link to your bookmark bar to easily bookmark them.</p>
+                <<< 'EOT'
+<p>For payment and an account, please contact the Office during office hours.</p>
+<p>For a quick logout, bookmark <a href="http://10.1.0.1:3990/logoff">LOGOUT</a>, this link will instantly log you out, and return you to the Welcome page.<br/>
+To get back to the status page, bookmark ether the Non javascript version (<a href="./nojsstatus" target="grasestatus">Hotspot Status nojs</a>), or the preferred javascript version (<a href="javascript: loginwindow = window.open('http://10.1.0.1/grase/uam/mini', 'grasestatus', 'width=300,height=400,location=no,directories=no,status=yes,menubar=no,toolbar=no'); loginwindow.focus();">Hotspot Status</a>). You can just drag ether link to your bookmark bar to easily bookmark them.</p>
 
-                <p>Your Internet usage is limit by the amount of data that flows to and from your computer, or the amount of time spent online (depending on what you account type is). To maximise your account, you may wish to do the following:</p>
-                <ul>
-                    <li>Browse with images turned off</li>
-                    <li>Resize all photos before uploading (800x600 is a good size for uploading to the internet, or emailing)</li>
-                    <li>Ensure antivirus programs do not attempt to update the program (you probably still want them to update the virus definition files).</li>
-                    <li>Use a client program for email instead of using webmail.</li>
-                    <li>Ensure when you finish using the Internet, you click logout so that other users won\'t be able to use your account</li>
-                </ul>
-                '
+<p>Your Internet usage is limit by the amount of data that flows to and from your computer, or the amount of time spent online (depending on what you account type is). To maximise your account, you may wish to do the following:</p>
+<ul>
+    <li>Browse with images turned off</li>
+    <li>Resize all photos before uploading (800x600 is a good size for uploading to the internet, or emailing)</li>
+    <li>Ensure antivirus programs do not attempt to update the program (you probably still want them to update the virus definition files).</li>
+    <li>Use a client program for email instead of using webmail.</li>
+    <li>Ensure when you finish using the Internet, you click logout so that other users won't be able to use your account</li>
+</ul>
+EOT
             );
             $this->rowsUpdated++;
         }
@@ -256,9 +258,10 @@ class Upgrade
         if ($Settings->getTemplate('loggedinnojshtml') === null) {
             $Settings->setTemplate(
                 'loggedinnojshtml',
-                '
-               <p>Your login was successful. Please click <a href="nojsstatus" target="grasestatus">HERE</a> to open a status window<br/>If you don\'t open a status window, then bookmark the link <a href="http://logout/">http://logout/</a> so you can logout when finished.</p>
-               '
+                <<<'EOT'
+<p>Your login was successful. Please click <a href="nojsstatus" target="grasestatus">HERE</a> to open a status window<br/>If you don't open a status window, then bookmark the link <a href="http://logout/">http://logout/</a> so you can logout when finished.</p>
+EOT
+
             );
             $this->rowsUpdated++;
         }
@@ -267,7 +270,7 @@ class Upgrade
         if ($Settings->getTemplate('ticketPrintCSS') === null) {
             $Settings->setTemplate(
                 'ticketPrintCSS',
-                '
+                <<<'EOT'
 body {
     line-height: 1.5;
     color: black;
@@ -300,7 +303,7 @@ body {
 #generated {
     display: none;
 }
-            '
+EOT
             );
             $this->rowsUpdated++;
         }
