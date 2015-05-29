93d70854f022f545bd8f00528494c903a256bd99
Tim White <tim@whiteitsolutions.com.au>
2015-05-29 12:51:56 +1000

    Remove old scripts that are no longer used

--- debian/grase-www-portal.cron.d
+++ debian/grase-www-portal.cron.d
@@ -6,12 +6,6 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 #MAILTO=
 ## NEEDS USERNAME
-#@daily	        root	/root/system/install/upgrade_script.sh
-#@reboot         root	/usr/share/grase/www/radmin/scripts/radius_stale > /dev/null
-#@daily          root	/usr/share/grase/www/radmin/scripts/radius_stale > /dev/null
-#3 5     1,2 * * root    /usr/share/grase/www/radmin/scripts/radius_stale > /dev/null
-#17 5	1,2 * * root    /usr/share/grase/www/radmin/scripts/radius_monthly_acct
-#37 3	3 * *	root    /usr/share/grase/www/radmin/scripts/radius_old_users
 @daily		    root    /usr/share/grase/www/radmin/scripts/mysql_backup
 
 # Most cron scripts have moved to PHP classes actived by cron.php
