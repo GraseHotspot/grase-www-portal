aa325bd39cf6ec582a951915918cace60b021584
Tim White <tim@whiteitsolutions.com.au>
2015-06-04 20:09:30 +1000

    Bring in AvanzuAdminThemeBundle to see how well it goes

--- files/usr/share/grase/symfony/app/AppKernel.php
+++ files/usr/share/grase/symfony/app/AppKernel.php
@@ -16,8 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            //new AppBundle\AppBundle(),
             new Grase\RadminBundle\GraseRadminBundle(),
+            new Avanzu\AdminThemeBundle\AvanzuAdminThemeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
