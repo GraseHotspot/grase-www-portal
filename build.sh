cb1b622d76209e3f86b554e680849f3075884a12
Tim White <tim@whiteitsolutions.com.au>
2017-01-15 08:03:32 +1000

    Typo in build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Script for building grase-www-portal in CI
 
 version=$(git describe |tr '-' '.')
-[[ ! -z $DISTRO ]] version="$version~$DISTRO"
+[[ ! -z $DISTRO ]] && version="$version~$DISTRO"
 echo Building $version
 dch -b -v $version "CI Build"
 dpkg-buildpackage -us -uc "$@"
