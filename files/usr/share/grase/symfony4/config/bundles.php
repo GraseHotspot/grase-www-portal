a282bb3ef7a1291431069ccc86908cba94f40bc9
Tim White <tim@whiteitsolutions.com.au>
2018-07-24 21:48:22 +1000

    Fixup apache and debugging

--- files/usr/share/grase/symfony4/config/bundles.php
+++ files/usr/share/grase/symfony4/config/bundles.php
@@ -11,4 +11,6 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
