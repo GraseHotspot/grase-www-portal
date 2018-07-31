b9139b1c2d8b8731c17f5c31006df3b72e9f4e55
Tim White <tim@whiteitsolutions.com.au>
2018-07-31 20:19:13 +1000

    Migrate dhcp leases page

--- files/usr/share/grase/symfony4/src/Controller/DefaultController.php
+++ files/usr/share/grase/symfony4/src/Controller/DefaultController.php
@@ -6,11 +6,13 @@ use App\Entity\Radius\Group;
 use App\Entity\Radius\User;
 use App\Entity\Radius\UserRepository;
 use App\Entity\Setting;
+use Grase\Util;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use App\Form\Radius\GroupType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
 {
@@ -105,5 +107,41 @@ class DefaultController extends Controller
                 'settings' => $settings
             ]
         );
+    }
+
+    public function dhcpLeasesAction(Session $session)
+    {
+        $chilliLeases = Util::getChilliLeases();
+        if ($chilliLeases && isset($chilliLeases['sessions'])) {
+            $sessions = $chilliLeases['sessions'];
+        } else {
+            $session->getFlashBag()->add(
+                'danger',
+                'Unable to fetch DHCP leases for Coova Chilli'
+            );
+            $sessions = [];
+        }
+
+        // Prewarm all users with a single query so we can get the comments as required
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+        foreach($sessions as $id => $session) {
+            if (isset($session['session']) && isset($session['session']['userName'])) {
+                /** @var User $user */
+                $user = $this->getDoctrine()->getRepository(User::class)->find($session['session']['userName']);
+                if ($user) {
+                    $sessions[$id]['session']['comment'] = $user->getComment();
+                }
+            }
+        }
+        // Make sure we "use" $users so it's not an unused variable
+        unset($users);
+
+        return $this->render(
+            'dhcpLeases.html.twig',
+            [
+                'sessions' => $sessions
+            ]
+        );
+
     }
 }
