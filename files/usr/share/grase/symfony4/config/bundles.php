ffb5a0bdaa42b249a71553bb35b4d0b0da4a898e
Tim White <tim@whiteitsolutions.com.au>
2019-01-02 14:08:27 +0800

    Update to use our fork of pd-menu

--- files/usr/share/grase/symfony4/config/bundles.php
+++ files/usr/share/grase/symfony4/config/bundles.php
@@ -14,6 +14,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Pd\MenuBundle\PdMenuBundle::class => ['all' => true],
     Shivas\VersioningBundle\ShivasVersioningBundle::class => ['all' => true],
+    Pd\MenuBundle\PdMenuBundle::class => ['all' => true],
 ];
