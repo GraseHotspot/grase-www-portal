8b5c9bc3a19e2970a090a9c73d1b3705bc41f879
Tim White <tim@whiteitsolutions.com.au>
2020-02-02 20:53:00 +0800

    Initial V3 migration command

---
+++ files/usr/share/grase/symfony4/src/Command/MigrateLegacyV3DatabaseCommand.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Command;
+
+use App\Util\GraseConsoleStyle;
+use App\Util\SqlFileImporter;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+/**
+ * This command takes 2 database backups (radius/radmin) and imports them into the current database
+ * overriding all current data. It then runs the required database migrations to take us to a V4
+ * database structure *
+ */
+class MigrateLegacyV3DatabaseCommand extends Command
+{
+    protected static $defaultName = 'grase:migrate-v3-backup';
+
+    /** @var TranslatorInterface */
+    private $translator;
+
+    /** @var SqlFileImporter */
+    private $sqlFileImporter;
+
+    /** @var LoggerInterface  */
+    private $logger;
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator, SqlFileImporter $sqlFileImporter, LoggerInterface $logger)
+    {
+        parent::__construct();
+        $this->translator     = $translator;
+        $this->sqlFileImporter = $sqlFileImporter;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function configure()
+    {
+        $this
+            ->setDescription('Imports V3 backup files overriding the current database')
+            ->addArgument('radius_file', InputArgument::REQUIRED, 'Location of Radius database Backup File')
+            ->addArgument('radmin_file', InputArgument::REQUIRED, 'Location of Radmin database Backup File')
+        ;
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new GraseConsoleStyle($input, $output);
+        $radiusFilename = $input->getArgument('radius_file');
+        $radminFilename = $input->getArgument('radmin_file');
+
+        // TODO add logger steps
+
+        if (!file_exists($radiusFilename)) {
+            $io->error($this->translator->trans('grase.command.migrate-v3.error.radiusFileMissing', ['filename' => $radiusFilename]));
+            return -1;
+        }
+        if (!file_exists($radminFilename)) {
+            $io->error($this->translator->trans('grase.command.migrate-v3.error.radminFileMissing', ['filename' => $radminFilename]));
+            return -1;
+        }
+
+        if (!$this->sqlFileImporter->confirmBackupFile($radminFilename)) {
+            $io->error($this->translator->trans('grase.command.migrate-v3.error.backupFileInvalid', ['filename' => $radminFilename]));
+            return -1;
+        }
+
+        if (!$this->sqlFileImporter->confirmBackupFile($radiusFilename)) {
+            $io->error($this->translator->trans('grase.command.migrate-v3.error.backupFileInvalid', ['filename' => $radiusFilename]));
+            return -1;
+        }
+
+        $io->caution($this->translator->trans('grase.command.migrate-v3.warning-override-database'));
+
+        $answer = $io->confirm($this->translator->trans('grase.command.migrate-v3.confirm-import'), false);
+
+        if (!$answer) {
+            $io->success($this->translator->trans('grase.command.migrate-v3.noChangesMade'));
+            return 0;
+        }
+
+        $io->note($this->translator->trans('grase.command.migrate-v3.startClearDatabase'));
+        $this->sqlFileImporter->eraseDatabase();
+
+        $io->note($this->translator->trans('grase.command.migrate-v3.startImportBackupFile', ['filename' => $radminFilename]));
+        $result = $this->sqlFileImporter->importSqlFile($radminFilename);
+        if (!$result) {
+            $io->error($this->translator->trans('grase.command.migrate-v3.error.importFailed'));
+            return -1;
+        }
+
+        $io->note($this->translator->trans('grase.command.migrate-v3.startImportBackupFile', ['filename' => $radiusFilename]));
+        $result = $this->sqlFileImporter->importSqlFile($radiusFilename);
+        if (!$result) {
+            $io->error($this->translator->trans('grase.command.migrate-v3.error.importFailed'));
+            return -1;
+        }
+
+        $io->success($this->translator->trans('grase.command.migrate-v3.success.completed'));
+
+        // Database migrations
+        $this->logger->info($this->translator->trans('grase.command.migrate-v3.startMigrations'));
+        $io->note($this->translator->trans('grase.command.migrate-v3.startMigrations'));
+
+        $cmd = $this->getApplication()->find('doctrine:migrations:migrate');
+        $cmdInput = new ArrayInput([
+            'command' => 'doctrine:migrations:migrate',
+        ]);
+        $cmdInput->setInteractive(false);
+
+        try {
+            $cmd->run($cmdInput, $output);
+        } catch (\Exception $e) {
+            $io->error($this->translator->trans(
+                'grase.command.migrate-v3.error.doctrineMigrations'
+            ));
+            $this->logger->error($this->translator->trans(
+                'grase.command.migrate-v3.error.doctrineMigrations'
+            ), [
+                'exception' => $e->getMessage(),
+            ]);
+            return -1;
+        }
+
+        // Settings validation
+        $this->logger->info($this->translator->trans('grase.command.migrate-v3.startSettingsValidate'));
+        $io->note($this->translator->trans('grase.command.migrate-v3.startSettingsValidate'));
+
+        $cmd = $this->getApplication()->find('grase:settings-validate');
+        $arguments = [
+            'command' => 'grase:settings-validate',
+        ];
+
+        try {
+            $cmd->run(new ArrayInput($arguments), $output);
+        } catch (\Exception $e) {
+            $io->error($this->translator->trans(
+                'grase.command.migrate-v3.error.settingsValidation'
+            ));
+            $this->logger->error($this->translator->trans(
+                'grase.command.migrate-v3.error.settingsValidation'
+            ), [
+                                     'exception' => $e,
+                                 ]);
+            return -1;
+        }
+
+        return 0;
+
+    }
+}
---
+++ files/usr/share/grase/symfony4/src/Util/SqlFileImporter.php
@@ -0,0 +1,170 @@
+<?php
+
+
+namespace App\Util;
+
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\FetchMode;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+
+class SqlFileImporter
+{
+    /** @var Connection */
+    protected $connection;
+
+    /** @var LoggerInterface */
+    protected $logger;
+
+    public function __construct(EntityManagerInterface $entityManager, LoggerInterface $logger)
+    {
+        $this->connection = $entityManager->getConnection();
+        $this->logger = $logger;
+    }
+
+    private function readFile($filename)
+    {
+        $handle = gzopen($filename, "r");
+
+        while(!gzeof($handle)) {
+            yield trim(gzgets($handle));
+        }
+
+        gzclose($handle);
+    }
+
+    /**
+     * Takes a SQL file and imports it into our database
+     *
+     * @param string $filename
+     *
+     * @return bool
+     * @throws \Doctrine\DBAL\ConnectionException
+     */
+    public function importSqlFile($filename)
+    {
+        $query = "";
+
+        $this->connection->beginTransaction();
+
+        foreach ($this->readFile($filename) as $line)
+        {
+            // Skip line if it's a mysqldump comment or empty
+            if (substr($line, 0, 2) === '--' || trim($line) == '') {
+                continue;
+            }
+
+            // Check for CREATE DATABASE and USE lines and skip them
+            // e.g. CREATE DATABASE IF NOT EXISTS `radius`;
+            // e.g. USE `radius`;
+            if (substr($line, 0, 15) === 'CREATE DATABASE' || substr($line, 0, 5) == 'USE `') {
+                continue;
+            }
+
+            // Add line to current query
+            $query .= $line;
+
+            // Check if we're at the end of a query
+            if (substr(trim($line), -1, 1) == ';') {
+                try {
+                    // Perform the Query
+                    $this->connection->exec($query);
+                } catch (DBALException $e) {
+                    $this->logger->error(
+                        "Query failed while importing $filename",
+                        ['message' => $e->getMessage(), 'code' => $e->getCode()]
+                    );
+                    $this->connection->rollBack();
+                    return false;
+                }
+
+                // Query was successful, clear $query for next round
+                $query = "";
+            }
+        }
+        $this->connection->commit();
+
+        return true;
+    }
+
+    /**
+     * Confirm that the provided backup file appears to be either a radius or radmin backup file
+     * We basically do the same as the actual importSqlFile logic, but just checking for some
+     * matching DROP TABLE lines
+     *
+     * @param $filename
+     *
+     * @return bool
+     */
+    public function confirmBackupFile($filename)
+    {
+        /**
+         * Check for the following lines, they should be near the top
+         * DROP TABLE IF EXISTS `mtotacct`;
+         * DROP TABLE IF EXISTS `adminlog`;
+         */
+        $searchQueries = [
+            'DROP TABLE IF EXISTS `mtotacct`;',
+            'DROP TABLE IF EXISTS `adminlog`;',
+        ];
+
+        $lineCount = 0;
+
+        $query = "";
+
+        foreach ($this->readFile($filename) as $line)
+        {
+            // Skip line if it's a mysqldump comment or empty
+            if (substr($line, 0, 2) === '--' || trim($line) == '') {
+                continue;
+            }
+
+            // Add line to current query
+            $query .= $line;
+            $lineCount ++;
+
+            // Check if we're at the end of a query
+            if (substr(trim($line), -1, 1) == ';') {
+                // Check the command is one of the ones we're looking for, then we can exit
+                if (in_array($query, $searchQueries)) {
+                    return true;
+                }
+                $query = "";
+            }
+
+            // Don't allow us to read the entire file looking for something that should be at the top
+            if ($lineCount >= 100) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Erase all tables in the Grase hotspot database to make room for the import
+     *
+     * @throws DBALException
+     * @throws \Doctrine\DBAL\ConnectionException
+     */
+    public function eraseDatabase()
+    {
+        $tables = $this->connection->query("SHOW TABLES")->fetchAll(FetchMode::COLUMN);
+        $this->connection->beginTransaction();
+
+        $this->connection->exec('SET FOREIGN_KEY_CHECKS=0;');
+
+        foreach ($tables as $table)
+        {
+            // TODO? Show which tables we've deleted?
+            $this->connection->exec("DROP TABLE ${table}");
+        }
+
+        $this->connection->exec('SET FOREIGN_KEY_CHECKS=1;');
+
+        $this->connection->commit();
+    }
+
+
+}
