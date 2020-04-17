707fb6bb10894a214c2b427fd5ea39e529c86d22
Tim White <tim@whiteitsolutions.com.au>
2020-04-17 23:54:44 +0800

    Remove web-server-bundle

--- files/usr/share/grase/symfony4/config/bundles.php
+++ files/usr/share/grase/symfony4/config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
