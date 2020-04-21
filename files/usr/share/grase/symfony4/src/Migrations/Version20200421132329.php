9d68f7fd3cdd14ffe425383dd56aea63731b1049
Tim White <tim@whiteitsolutions.com.au>
2020-04-21 21:25:13 +0800

    Password upgrade field

---
+++ files/usr/share/grase/symfony4/src/Migrations/Version20200421132329.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Ensure password field is long enough for automatic hash upgrades
+ */
+final class Version20200421132329 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE auth CHANGE password password VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE auth CHANGE password password VARCHAR(60) NOT NULL COLLATE');
+    }
+}
