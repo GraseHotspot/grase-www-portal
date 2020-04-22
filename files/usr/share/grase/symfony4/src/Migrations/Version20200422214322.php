376d0795d85bf409a640ae33b1d956704ec80452
Tim White <tim@whiteitsolutions.com.au>
2020-04-22 21:45:37 +0800

    DB upgrade to cleanup group tables

---
+++ files/usr/share/grase/symfony4/src/Migrations/Version20200422214322.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Cleanup leftover groups in radgroupcheck and radgroupreply
+ */
+final class Version20200422214322 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DELETE from radgroupreply WHERE GroupName NOT IN (SELECT id from groups)');
+        $this->addSql('DELETE from radgroupcheck WHERE GroupName NOT IN (SELECT id from groups)');
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
