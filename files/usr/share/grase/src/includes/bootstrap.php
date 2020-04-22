a99cee85ac8dd2854360591b90622f4d06504b0d
Tim White <tim@whiteitsolutions.com.au>
2020-04-22 22:19:17 +0800

    Remove old files

--- files/usr/share/grase/src/includes/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-//$DatabaseConnections = new DatabaseConnections();
-//$DatabaseFunctions = new DatabaseFunctions($DatabaseConnections);
--- files/usr/share/grase/src/includes/grase-bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/*
- * This file is for bootstrapping the bare minimum needed for GRASE, and only what can't be done via the vendor
- * autoloader. All parts of the GRASE system should rely on the vendor/autoload.php and not this file
- */
-
-// Needed for T_() function for translation of strings
-require_once('php-gettext/gettext.inc');
